8b1b087f069447d01a6598e0f45a1e57ac59953e
elsayedsameh803-star <elsayedsameh803@gmail.com>
2026-09-02 08:20:38 +0300

    chore: remove diagnostic scripts and deploy log artifacts from repository; ignore _* temp files

--- build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "c:\Users\Fast\OneDrive\Desktop\new smart land"
-echo BUILD_START:%date% %time% > build.log
-call npm run build >> build.log 2>&1
-echo BUILD_END:%date% %time% >> build.log
-
